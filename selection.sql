325dd19a9647859899d7302005992b03a9b26726
Türkalp Burak KAYRANCIOĞLU <bkayranci@gmail.com>
2018-05-08 01:22:03 +0300

    insertion gelistirildi

--- selection.sql
+++ selection.sql
@@ -9,9 +9,9 @@ WHERE BaslangicTarihi < '2018-06-01'
 /* otel olanaklarini fiyatlariyla yazar */
 SELECT OO.Ad, Fiyat.Deger FROM Fiyat
 	JOIN OtelOlanaklari AS OO ON OO.Id = Fiyat.OtelOlanaklariId
-WHERE BaslangicTarihi < '2018-06-01'
+WHERE BaslangicTarihi < '2019-06-01'
 
-/* oda tiplerinin verilen tarihler araliginda kac farkli fiyati oldugunu verir */
+/* oda tiplerinin verilen tarihler araliginda kac farkli fiyati oldugunu verir yani odanin fiyati kac defa degismis */
 SELECT OT.Ad AS Tipi, COUNT(*) FROM Fiyat
 	JOIN OdaTipi AS OT ON OT.Id = Fiyat.OdaTipiId
 WHERE 
