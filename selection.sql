d8bcc782e00a87880ffa89741ac1952d12bdabeb
Türkalp Burak KAYRANCIOĞLU <bkayranci@gmail.com>
2018-05-11 04:49:08 +0300

    son

--- selection.sql
+++ selection.sql
@@ -49,82 +49,27 @@ GROUP BY M.Cinsiyet, YEAR(Rez.BaslangicTarihi)
 ORDER BY YIL DESC, M.Cinsiyet DESC
 
 
+-- #4 @verilen sorgu
+-- Yıl, ay ve oda tipi bazında toplam fatura tutarı ve rezervasyon sayısı
+SELECT YEAR(RO.OlusturmaTarihi) AS [Yıl], MONTH(RO.OlusturmaTarihi) AS [Ay], OT.Ad AS [Oda Tipi], SUM(RO.ToplamTutar) AS [Fatura Tutarı], COUNT(OT.Ad) AS [Kaç Tane] FROM Rezervasyon Rez
+	JOIN Rezervasyon_Oda RO ON RO.Id = Rez.Id
+	JOIN Oda O ON O.Id = RO.OdaId
+	JOIN OdaTipi OT ON OT.Id = O.OdaTipiId
+GROUP BY YEAR(RO.OlusturmaTarihi), MONTH(RO.OlusturmaTarihi), OT.Ad
 
 
-
--- bitmedi
---  2018 Mart ayındaki rezervasyonlarda rez.süresi 2017 yılı ilk 3 aylık süreden daha uzun olan rezervasyonların bilgileri
-SELECT DATEDIFF(DD, Rez.BaslangicTarihi, Rez.BitisTarihi) AS RezervasyonSuresi, Mus.Ad + ' ' + Mus.Soyad AS RezervasyonYapanMusteri FROM Rezervasyon Rez
-JOIN Musteri Mus ON Mus.Id = Rez.MusteriId
+-- #5 @verilen sorgu
+-- 2018 Mart ayındaki rezervasyonlarda rez.süresi 2017 yılı ilk 3 aylık süreden daha uzun olan rezervasyonların bilgileri
+SELECT DATEDIFF(DD, Rez.BaslangicTarihi, Rez.BitisTarihi) AS RezervasyonSuresi, Mus.Ad + ' ' + Mus.Soyad AS RezervasyonYapanMusteri, Rez.BaslangicTarihi AS [Rezervasyon Başlangıç Tarihi], Rez.BitisTarihi AS [Rezervasyon Bitiş Tarihi] FROM Rezervasyon Rez
+	JOIN Musteri Mus ON Mus.Id = Rez.MusteriId
 	WHERE
 
 	-- baslangic ve bitis tarihi 2018 mart ayinda olan rezervasyonlar
 	(Rez.BaslangicTarihi BETWEEN '2018-03-01' AND '2018-03-31') AND (Rez.BitisTarihi BETWEEN '2018-01-01' AND '2018-03-31')
-
 	AND
 
-	-- ortalama sure yap
 	-- rezervasyon suresi, baslangic ve bitis tarihi 2017 ilk 3 ayda olan rezervasyonlarin suresinin her birinden buyuk olma kosulu
-	DATEDIFF(DD, Rez.BaslangicTarihi, Rez.BitisTarihi) > ALL(
+	DATEDIFF(DD, Rez.BaslangicTarihi, Rez.BitisTarihi) > 
 
-		-- baslangic ve bitis tarihi 2017 ilk 3 ayda olan rezervasyonlarin suresi
-		SELECT DATEDIFF(DD, R.BaslangicTarihi, R.BitisTarihi) AS RezervasyonSuresi FROM Rezervasyon AS R WHERE R.BaslangicTarihi BETWEEN '2017-01-01' AND '2017-03-31' AND R.BitisTarihi BETWEEN '2017-01-01' AND '2017-03-31'
-	)
-
-
-
--- Yıl, ay ve oda tipi bazında toplam fatura tutarı ve rezervasyon sayısı
-
-	-- rezervasyondaki odalarin toplam fiyati
-	SELECT SUM(F.Deger) FROM Oda O, OdaTipi OT, Rezervasyon_Oda RO, Rezervasyon Rez, Fiyat F
-	WHERE O.OdaTipiId = OT.Id AND RO.OdaId = O.Id AND RO.RezervasyonId= Rez.Id AND Rez.BaslangicTarihi BETWEEN F.BaslangicTarihi AND F.BitisTarihi
-	GROUP BY Rez.Id
-
--- rezervasyonun fatura tutarı yani rezervasyondaki odalarin, ek hizmetlerin ve otel olanaklarin rezervasyonun baslangic tarihindeki fiyatlarinin toplami
-SELECT TUMTOPLAMLAR.RezId, SUM(TUMTOPLAMLAR.TOPLAMFIYAT) AS TOPLAMFATURATUTARI FROM (
-	-- rezervasyondaki odalarin toplam tutari
-	SELECT Rez.Id AS RezId, SUM(F.Deger) AS TOPLAMFIYAT FROM Rezervasyon Rez
-	JOIN Rezervasyon_Oda RO ON RO.RezervasyonId = Rez.Id
-	JOIN Oda O ON O.Id = RO.OdaId
-	JOIN OdaTipi OT ON OT.Id = O.OdaTipiId
-	JOIN Fiyat F ON F.OdaTipiId = OT.Id AND Rez.BaslangicTarihi BETWEEN F.BaslangicTarihi AND F.BitisTarihi
-	GROUP BY Rez.Id
-
-	UNION
-	
-	-- rezervasyondaki ek hizmetlerin toplam tutari
-	SELECT Rez.Id AS RezId, SUM(F.Deger) AS TOPLAMFIYAT FROM Rezervasyon Rez
-	JOIN Rezervasyon_EkHizmet REK ON REK.RezervasyonId = Rez.Id
-	JOIN EkHizmet EK ON EK.Id = REK.EkHizmetId
-	JOIN Fiyat F ON F.EkHizmetId = EK.Id AND Rez.BaslangicTarihi BETWEEN F.BaslangicTarihi AND F.BitisTarihi
-	GROUP BY Rez.Id
-
-	UNION
-
-	-- otel olanagını ekleme sadece kullanılırken odeme yapılsın
-	-- rezervasyondaki otel olanaklarinin toplam tutari
-	SELECT Rez.Id AS RezId, SUM(F.Deger) AS TOPLAMFIYAT FROM Rezervasyon Rez
-	JOIN Rezervasyon_OtelOlanaklari ROO ON ROO.RezervasyonId = Rez.Id
-	JOIN OtelOlanaklari OO ON OO.Id = ROO.OtelOlanaklariId
-	JOIN Fiyat F ON F.EkHizmetId = OO.Id AND Rez.BaslangicTarihi BETWEEN F.BaslangicTarihi AND F.BitisTarihi
-	GROUP BY Rez.Id) AS TUMTOPLAMLAR
-GROUP BY TUMTOPLAMLAR.RezId
-
-
--- bitmedi
--- ## V2 ## rezervasyonun fatura tutarı yani rezervasyondaki odalarin, ek hizmetlerin ve otel olanaklarin rezervasyonun baslangic tarihindeki fiyatlarinin toplami
-SELECT TUMTABLO.Id, SUM(TUMTABLO.TOPLAMTUTAR) AS TOPLAMFATURATUTARI FROM
-(SELECT Rez.Id, SUM(REK.ToplamTutar) AS TOPLAMTUTAR FROM Rezervasyon Rez
-	JOIN Rezervasyon_EkHizmet REK ON REK.RezervasyonId = Rez.Id
-	GROUP BY Rez.Id
-UNION
-SELECT Rez.Id, SUM(ROO.ToplamTutar) AS TOPLAMTUTAR FROM Rezervasyon Rez
-	JOIN Rezervasyon_OtelOlanaklari ROO ON ROO.RezervasyonId = Rez.Id
-	GROUP BY Rez.Id
-UNION
-SELECT Rez.Id, SUM(RO.ToplamTutar) AS TOPLAMTUTAR FROM Rezervasyon Rez
-	JOIN Rezervasyon_Oda RO ON RO.RezervasyonId = Rez.Id
-	GROUP BY Rez.Id
-) AS TUMTABLO
-GROUP BY TUMTABLO.Id
-	
+		-- baslangic ve bitis tarihi 2017 ilk 3 ayda olan rezervasyonlarin suresinin ortalamasi
+		(SELECT AVG(DATEDIFF(DD, R.BaslangicTarihi, R.BitisTarihi)) AS RezervasyonSuresiOrtalamasi FROM Rezervasyon AS R WHERE R.BaslangicTarihi BETWEEN '2017-01-01' AND '2017-03-31' AND R.BitisTarihi BETWEEN '2017-01-01' AND '2017-03-31')
